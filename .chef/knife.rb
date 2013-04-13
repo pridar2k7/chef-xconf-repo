58ead0fed3a0a1a41592f2b0b0b5618480363062
Admin <admin@Admins-MacBook-Pro.local>
2013-04-13 12:31:26 +0530

    Changed path of pem in knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'chef-client'
-client_key               '/home/ubuntu/.chef/chef-client.pem'
+client_key               'chef-client.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/ubuntu/.chef/validation.pem'
+validation_key           'validation.pem'
 chef_server_url          'http://54.214.2.155:4000'
 cache_type               'BasicFile'
-cache_options( :path => '/home/ubuntu/.chef/checksums' )
+cache_options( :path => 'checksums' )
