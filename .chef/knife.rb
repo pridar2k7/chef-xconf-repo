a4001f32bd15561562883ec3ab59b62496765ae5
Admin <admin@Admins-MacBook-Pro.local>
2013-04-13 16:15:16 +0530

    modified knife.rb, updated java cookbook, created a valid databag and node

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'chef-client'
-client_key               'chef-client.pem'
+client_key               '/Users/admin/chef/chef-repo/chef-xconf-repo/.chef/chef-client.pem'
 validation_client_name   'chef-validator'
-validation_key           'validation.pem'
+validation_key           '/Users/admin/chef/chef-repo/chef-xconf-repo/.chef/validation.pem'
 chef_server_url          'http://54.214.2.155:4000'
-cache_type               'BasicFile'
-cache_options( :path => 'checksums' )
+syntax_check_cache_path  '/Users/admin/chef/chef-repo/chef-xconf-repo/.chef/syntax_check_cache'
+cookbook_path [ './cookbooks' ]
