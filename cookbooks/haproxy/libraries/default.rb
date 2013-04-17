47a3b2cdcebec1715d21615dad44cbc3ae0e6693
sharanya <sharans@thoughtworks.com>
2013-04-18 00:18:39 +0530

    configured haproxy to search nodes based on ip_address for role:QA

---
+++ cookbooks/haproxy/libraries/default.rb
@@ -0,0 +1,15 @@
+def defaults_options
+  options = node['haproxy']['defaults_options']
+  if node['haproxy']['x_forwarded_for']
+    options.push("forwardfor")
+  end
+  return options.uniq
+end
+
+def defaults_timeouts
+  node['haproxy']['defaults_timeouts']
+end
+
+def app_nodes
+  search(:node, "role:QA")
+end
