74632b29510545de1021ca678f31a579d6381eec
sharanya <sharans@thoughtworks.com>
2013-04-18 23:20:44 +0530

    Sharanya - updated knife.rb with relative reference to pem files

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'chef-client'
-client_key               '/Users/admin/chef/chef-repo/chef-xconf-repo/.chef/chef-client.pem'
+client_key               './chef-client.pem'
 validation_client_name   'chef-validator'
-validation_key           '/Users/admin/chef/chef-repo/chef-xconf-repo/.chef/validation.pem'
+validation_key           './validation.pem'
 chef_server_url          'http://54.214.2.155:4000'
-syntax_check_cache_path  '/Users/admin/chef/chef-repo/chef-xconf-repo/.chef/syntax_check_cache'
+syntax_check_cache_path  './syntax_check_cache'
 cookbook_path [ './cookbooks' ]
