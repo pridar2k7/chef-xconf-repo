8e008561a6b42eef168b259d2b395b27e14ef21b
sharanya <sharans@thoughtworks.com>
2013-04-19 22:21:42 +0530

    Sharanya - bootstrap, knife, and node info

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@ log_location             STDOUT
 node_name                'chef-client'
 client_key               './chef-client.pem'
 validation_client_name   'chef-validator'
-validation_key           './validation.pem'
+validation_key           './.chef/validation.pem'
 chef_server_url          'http://54.214.2.155:4000'
 syntax_check_cache_path  './syntax_check_cache'
 cookbook_path [ './cookbooks' ]
